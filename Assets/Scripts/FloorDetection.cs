62a3790f302fe17d1f600b088b46cd354b0d7e35
Solene210 <jahn.solene@gmail.com>
2023-03-27 14:37:55 +0200

    Mouvement player
    Il peut sauter, courir et marcher

---
+++ Assets/Scripts/FloorDetection.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorDetection : MonoBehaviour
+{
+    #region Expose
+    [SerializeField] private Transform[] _rayOrigins;
+    [SerializeField] private float _rayLenght;
+    [SerializeField] private LayerMask _groundMask;
+    #endregion
+
+    #region Unity Life Cycle
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+
+        foreach (Transform origin in _rayOrigins)
+        {
+            Gizmos.DrawRay(origin.position, Vector3.down * _rayLenght);
+        }
+    }
+    #endregion
+
+    #region methods
+    public Vector3 AverageHeight()
+    {
+        int hitCount = 0;
+        Vector3 combinedPosition = Vector3.zero;
+        RaycastHit hit;
+        foreach (Transform origin in _rayOrigins)
+        {
+            if (Physics.Raycast(origin.position, Vector3.down, out hit, _rayLenght, _groundMask))
+            {
+                hitCount++;
+                //hit.point : La position dans le world où le Raycast a touché le collider
+                combinedPosition += hit.point;
+            }
+        }
+        Vector3 averagePosition = Vector3.zero;
+        if (hitCount > 0)
+        {
+            averagePosition = combinedPosition / hitCount;
+        }
+        return averagePosition;
+    }
+    #endregion
+
+    #region Private & Protected
+
+    #endregion
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,17 @@ public class PlayerMovement : MonoBehaviour
     #region Expose
     [Header("Movement Parameters")]
     [SerializeField] private int _walkSpeed;
+    [SerializeField] private int _sprintSpeed;
+    [SerializeField] private float _jumpForce;
+    [Space]
+    [Header("Floor Detection")]
+    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private Vector3 _boxDimension;
+    [SerializeField] private Transform _groundChecker;
+    [SerializeField] private float _yFloorOfset;
+    [Space]
+    [Header("Slope handling")]
+    [SerializeField] private float _maxSlopeAngle;
     #endregion
 
     #region Unity Life Cycle
@@ -19,25 +30,73 @@ public class PlayerMovement : MonoBehaviour
     {
         _cameraTransform = Camera.main.transform;
         _rigidbody = GetComponent<Rigidbody>();
+        _floorDetector = GetComponentInChildren<FloorDetection>();
     }
 
     void Update()
     {
-        Walk();
+        Move();
+        Jump();
+        Collider[] groundColliders = Physics.OverlapBox(_groundChecker.position, _boxDimension, Quaternion.identity, _groundMask);
+        _isGrounded = groundColliders.Length > 0;
     }
 
     private void FixedUpdate()
     {
+        if (_isGrounded)
+        {
+            StickToGround();
+            if (_isJumping)
+            {
+                _isGrounded = false;
+                _direction.y = _jumpForce;
+                _isJumping = false;
+            }
+        }
+        else
+        {
+            _direction.y = _rigidbody.velocity.y;
+        }
+        if (SlopeAngle() > _maxSlopeAngle)
+        {
+            Vector3 localDirection = transform.InverseTransformDirection(_direction);   //Passe du gloabal au local
+            if (localDirection.z > 0) localDirection.z = 0;   //Pas le droit d'avancer
+            _direction = transform.TransformDirection(localDirection);  //Repasse la direction en global
+        }
         _rigidbody.velocity = _direction;
         RotateTowardsCamera();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(_groundChecker.position, _boxDimension * 2f);
     }
     #endregion
 
     #region methods
-    private void Walk()
+    private void Move()
     {
         _direction = _cameraTransform.right * Input.GetAxis("Horizontal") + _cameraTransform.forward * Input.GetAxis("Vertical");
         _direction *= _walkSpeed;
+        if (Input.GetButton("Sprint"))
+        {
+            Sprint();
+        }
+    }
+
+    private void Sprint()
+    {
+        _direction = _cameraTransform.right * Input.GetAxis("Horizontal") + _cameraTransform.forward * Input.GetAxis("Vertical");
+        _direction *= _sprintSpeed;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _isJumping = true;
+        }
     }
 
     private void RotateTowardsCamera()
@@ -47,9 +106,27 @@ public class PlayerMovement : MonoBehaviour
             Vector3 cameraForward = _cameraTransform.forward;
             cameraForward.y = 0;
             Quaternion lookRotation = Quaternion.LookRotation(cameraForward);
-            //Quaternion rotation = Quaternion.RotateTowards(_rigidbody.rotation, lookRotation, _turnSpeed * Time.fixedDeltaTime);
             _rigidbody.MoveRotation(lookRotation);
         }
+    }
+
+    private void StickToGround()
+    {
+        Vector3 averagePosition = _floorDetector.AverageHeight();
+        Vector3 newPosition = new Vector3(_rigidbody.position.x, averagePosition.y + _yFloorOfset, _rigidbody.position.z);
+        _rigidbody.MovePosition(newPosition);
+    }
+
+    private float SlopeAngle()
+    {
+        Vector3 startingpoint = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.5f);
+        Debug.DrawRay(startingpoint, Vector3.down);
+        if (Physics.Raycast(startingpoint, Vector3.down, out _slopeHit))
+        {
+            float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
+            return angle;
+        }
+        return 370;
     }
     #endregion
 
@@ -57,5 +134,9 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 _direction = new Vector3();
     private Transform _cameraTransform;
     private Rigidbody _rigidbody;
+    private FloorDetection _floorDetector;
+    private RaycastHit _slopeHit;
+    public bool _isJumping = false;
+    public bool _isGrounded = true;
     #endregion
 }
